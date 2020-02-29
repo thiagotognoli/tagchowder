0bd29d315b66a6d72bcd98ccbfaff0fc3a72fe8c
Thiago Tognoli <thiagotognoli@gmail.com>
2020-02-29 02:24:47 -0300

    Created package.sh with docker

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#docker run -it --rm --volume "$(pwd)/pom.xml"://usr/src/app/pom.xml maven:3-jdk-8-alpine mvn install
+
+docker run -it --rm --name tagsoup --volume "$(pwd)"/.m2:/root/.m2 -v "$(pwd)":/usr/src/tagsoup -w /usr/src/tagsoup maven:3.3-jdk-8 mvn clean install 
+mkdir -p dist \
+&& cp core/target/tagchowder.core.jar dist/.
+
+#--volume "$(pwd)"/.m2:/root/.m2
+#-u $(id -u):$(id -g)
