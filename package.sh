fe991ed223a6dc65daf4e6305167683e0430900a
Thiago Tognoli <thiagotognoli@gmail.com>
2020-02-29 03:13:01 -0300

    jar with dependencies

--- package.sh
+++ package.sh
@@ -2,9 +2,13 @@
 
 #docker run -it --rm --volume "$(pwd)/pom.xml"://usr/src/app/pom.xml maven:3-jdk-8-alpine mvn install
 
-docker run -it --rm --name tagsoup --volume "$(pwd)"/.m2:/root/.m2 -v "$(pwd)":/usr/src/tagsoup -w /usr/src/tagsoup maven:3.3-jdk-8 mvn clean install 
+docker run -it --rm --name tagchowder \
+        --volume "$(pwd)"/.m2:/root/.m2 \
+        -v "$(pwd)":/usr/src/tagchowder \
+        -w /usr/src/tagchowder maven:3.3-jdk-8 \
+        mvn clean compile assembly:single 
 mkdir -p dist \
-&& cp core/target/tagchowder.core.jar dist/.
+&& cp core/target/tagchowder.core-jar-with-dependencies.jar dist/tagchowder.core.jar
 
 #--volume "$(pwd)"/.m2:/root/.m2
 #-u $(id -u):$(id -g)
